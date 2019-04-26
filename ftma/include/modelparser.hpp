9b8a337c22d904009ee1c61e1b3e1cd663738af7
yun <dlyun2009@gmail.com>
2019-04-26 11:36:14 +0800

    change class name to captical

---
+++ ftma/include/modelparser.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file   modelparser.hpp
+ * @author Liyun Dai <dlyun2009@gmail.com>
+ * @date   Thu Apr 25 18:31:08 2019
+ *
+ * @brief  xml model parser
+ *
+ *
+ */
+#ifndef __MODEL_PARSER_HPP
+#define __MODEL_PARSER_HPP
+
+namespace ftma {
+using namespace std;
+
+}
+
+#endif
