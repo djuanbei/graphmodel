15235bf8aaa10421f2864dc60f87ad6db05d7c13
Liyun Dai <dailiyun@swu.edu.cn>
2019-06-19 21:47:26 +0800

    ok

--- ftma/include/util/datacompression.h
+++ ftma/include/util/datacompression.h
@@ -18,35 +18,62 @@ namespace graphsat {
 typedef unsigned int uint;
 using std::numeric_limits;
 using std::pair;
+using std::make_pair;
 using std::vector;
+
 
 class Compression {
 public:
   Compression( int len )
-      : bounds( len )
-      , nbit( len, sizeof( uint ) ) {
+      :row_len( len), bounds( len )
+      , nbit( len, sizeof( uint ) ), uintValue( len),intValue( len), shift( len) {
     for ( int i = 0; i < len; i++ ) {
       bounds[ i ].first  = numeric_limits<int>::min();
       bounds[ i ].second = numeric_limits<int>::max();
+      shift[ i]=make_pair( i, 0);
     }
-    bitLen = len * sizeof( uint );
   }
+  
   void setBound( int id, int low, int up ) {
     assert( up > low );
     bounds[ id ].first  = low;
     bounds[ id ].second = up;
     uint bound          = up - low;
     int  len            = log2( bound );
+    uint temp=1<<len;
+    if( bound>temp){
+      len++;
+    }
+    nbit[ id]=len;
+  }
+  void update( ){
+    int uintId=0;
+    int  temp=0; 
+    int uint_size=sizeof( uint);
+    
+    for( int i=0; i<row_len; i++ ){
+      if( temp+nbit[ i]>=uint_size){
+        uintId++;
+        temp=0; 
+      }
+      shift[ i]=make_pair( uintId, temp);
+      temp+=nbit[i];
+    }
   }
 
-  uint *encode( int *data ) const;
+  uint *encode( int *data ) ;
 
-  int *decode( uint *data ) const;
+  int *decode( uint *data ) ;
 
 private:
+  int row_len;
   vector<pair<int, int>> bounds;
   vector<int>            nbit;
-  int                    bitLen = 0;
+  vector<uint> uintValue;
+  vector<int> intValue;
+  vector<pair<int, int> > shift;
+
+
 };
 } // namespace graphsat
 
--- ftma/src/datacompression.cpp
+++ ftma/src/datacompression.cpp
@@ -1,8 +1,29 @@
 #include "util/datacompression.h"
 
 namespace graphsat {
-uint *Compression::encode( int *data ) const { return NULL; }
 
-int *Compression::decode( uint *data ) const { return NULL; }
+uint *Compression::encode( int *data ) {
+  fill( uintValue.begin( ), uintValue.end( ), 0);
+  
+  for( int i=0;i< row_len; i++){
+    assert( data[ i]>=bounds[ i].first);
+    assert( data[ i]<=bounds[ i].second);
+    uint temp=data[ i]-bounds[ i].first;
+    temp<<=shift[ i].second;
+    uintValue[shift[ i].first]+=temp;
+  }
+  return &(uintValue[ 0]);
+}
+
+int *Compression::decode( uint *data )  {
+  for( int i=0; i< row_len; i++){
+    uint temp=data[shift[ i].first ];
+    int high=shift[ i].second+nbit[ i];
+    temp<<=sizeof( uint)-high;
+    temp>>=(sizeof( uint)-nbit[ i]);
+    intValue[ i]=temp;
+  }
+  return &( intValue[ 0]);
+}
 
 } // namespace graphsat
