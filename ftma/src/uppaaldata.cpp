a2c41b577dd33f9e69845d9d9301fc663f7d6e7b
yun <dlyun2009@gmail.com>
2019-10-22 15:12:54 +0800

    ok

---
+++ ftma/src/uppaaldata.cpp
@@ -0,0 +1,63 @@
+#include "io/uppaaldata.h"
+
+namespace graphsat{
+
+UppaalData::UppaalData() {
+  init_loc       = 0;
+  name           = "";
+  global_var_num = 0;
+  parent         = NULL;
+  TYPE_MAP( type_to_name, INT_T );
+  base_types.push_back(INT_T );
+  TYPE_MAP( type_to_name, CLOCK_T );
+  base_types.push_back(CLOCK_T );
+  TYPE_MAP( type_to_name, BOOL_T );
+  base_types.push_back(BOOL_T );
+    
+  TYPE_MAP( type_to_name, CHAN_T );
+  TYPE_MAP( type_to_name, URGENT_CHAN_T );
+  TYPE_MAP( type_to_name, BROADCAST_CHAN_T );
+  base_types.push_back(CHAN_T );
+
+  type_to_name[ SYSTEM_T ]    = STRING( SYSTEM_T );
+  base_types.push_back(SYSTEM_T  );
+    
+  type_to_name[ PARAMETER_T ] = STRING( PARAMETER_T );
+  base_types.push_back( PARAMETER_T );
+        
+  type_to_name[ TEMPLATE_T ]  = STRING( TEMPLATE_T );
+  base_types.push_back( TEMPLATE_T );
+    
+  type_to_name[ LOCATION_T ]  = STRING( LOCATION_T );
+
+        
+  type_to_name[ CLOCK_CS_T ]  = STRING( CLOCK_CS_T );
+  base_types.push_back( CLOCK_CS_T );
+  type_to_name[ INT_CS_T ]    = STRING( INT_CS_T );
+  base_types.push_back( INT_CS_T );
+    
+  type_to_name[ INT_UPDATE_T ] = type_to_name[ INT_UPDATE_T ];
+  type_to_name[ RESET_T ]      = type_to_name[ RESET_T ];
+
+  type_to_name[ NO_T ] = STRING( NO_T );
+}
+
+
+TYPE_T UppaalData::getType( const string & name) const{
+  for( vector<TYPE_T>::const_iterator it= base_types.begin( ); it!= base_types.end( ); it++ ){
+    if(hasPointer(*it, name ) ){
+      return *it;
+    }
+    if( hasValue(*it, name)){
+      return *it;
+    }
+  }
+  
+  if( NULL!= parent){
+    return parent->getType( name);
+  }
+
+  return NO_T;
+  
+}
+}
