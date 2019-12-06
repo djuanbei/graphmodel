e071753f57bd9f09b0fc57bb568fd4c74e8b7cc7
yun <dlyun2009@gmail.com>
2019-12-06 11:26:43 +0800

    ok

---
+++ ftma/src/logset.cpp
@@ -0,0 +1,19 @@
+
+#include "log/logset.h"
+
+namespace graphsat {
+void logSet() {
+  FLAGS_alsologtostderr = 1;
+  google::SetStderrLogging(
+      google::
+          GLOG_INFO ); // 设置glog的输出级别，这里的含义是输出INFO级别以上的信息
+  // 设置INFO级别以上的信息log文件的路径和前缀名
+  //  google::SetLogDestination(google::GLOG_INFO, "~/log/INFO_");
+
+  // 设置WARNING级别以上的信息log文件的路径和前缀名
+  // google::SetLogDestination(google::GLOG_WARNING, "~/log/WARNING_");
+
+  // 设置ERROR级别以上的信息log文件的路径和前缀名
+  // google::SetLogDestination(google::GLOG_ERROR, "~/log/ERROR_");
+}
+} // namespace graphsat
